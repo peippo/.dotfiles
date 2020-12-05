e81bc3d476e62f214b05705360a5681fb801dd02
Janne Peippo <janne.peippo@poutapilvi.fi>
2020-12-05 14:40:16 +0200

    Add vscode settings

---
+++ vscode/install.sh
@@ -0,0 +1,15 @@
+code --install-extension dracula-theme.theme-dracula
+code --install-extension esbenp.prettier-vscode
+code --install-extension eamodio.gitlens
+code --install-extension wix.vscode-import-cost
+code --install-extension zignd.html-css-class-completion
+code --install-extension eriklynd.json-tools
+code --install-extension hazer.reactcodesnippets
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension jpoissonnier.vscode-styled-components
+
+
+
+
+
+
