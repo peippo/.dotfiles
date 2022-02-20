376b0464b74b5b274e7281fcd7d43b7114591ac9
Janne Peippo <janne.peippo@poutapilvi.fi>
2022-02-20 12:23:40 +0200

    Add VS Code plugins

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,6 +7,10 @@ code --install-extension eriklynd.json-tools
 code --install-extension hazer.reactcodesnippets
 code --install-extension wayou.vscode-todo-highlight
 code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension svelte.svelte-vscode
+code --install-extension ms-vscode.vscode-typescript-next
 
 
 
